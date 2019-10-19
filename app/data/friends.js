8a4f18603f3c14d936332ae10a6d7506736b1e87
Rafael De Leon <rafael_de_leon88@hotmail.com>
2019-10-19 15:03:58 -0400

    apiroutes working

---
+++ app/data/friends.js
@@ -0,0 +1,15 @@
+var friendsArray = [
+  {
+    name: "Goku",
+    photo:
+      "https://www.geek.com/wp-content/uploads/2018/06/goku_large-625x352.png",
+    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+  },
+  
+  name: "Vegeta",
+  photo:
+    "hhttps://freeaddon.com/wp-content/uploads/2017/10/vegeta-5.jpg",
+  scores: ["5", "5", "2", "3", "5", "1", "6", "8", "3", "9"]
+];
+
+module.exports = friendsArray;
